46214449014c8de92508bf5fb6d2e329b840ec49
Aryan Kumar <adaryan0811@gmail.com>
2024-01-05 12:50:06 +0530

    feat: Add initial project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Diseases from './pages/Diseases';
+import Contact from './pages/Contact';
+import Diagnose from './pages/Diagnose';
+import Header from './component/Header';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/diseases" element={<Diseases />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/diagnose" element={<Diagnose />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
